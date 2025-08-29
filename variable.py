2fac6fde0a4ae5f4b03e4490192fbc17c5a6054f
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-08-29 13:27:33 -0600

    Variable notes

---
+++ variable.py
@@ -0,0 +1,35 @@
+# VL 6th Variable notes
+
+# what is one way to give a program an output
+print("this is an output!")
+
+# Variable
+subject = "CSP"
+pi=3.14
+color = "purple"
+print(pi)
+
+#snake-case (all lower case,space replaced witj underscroes)"baseball_team
+baseball_team = "Red sox"
+#camelCase (2nd word and on caltilize, squash words together)
+baseballTeam = "pirates"
+#PascalCase (captialize the 1st letter of every word, squish words together)
+BaseballTeam = "Giants" 
+
+#resign a variable
+pi= 3.14159265
+
+pi= "French Silk"
+
+pi= "Pumpkin"
+
+print(pi)
+
+name = input("what is your name?")
+
+quest = input("what is your quest?")
+
+print("what is the average wing velocity of a unlaiden sollow?")
+velocity = input()
+
+print("your name is", name, "this is", subject,  "the best pie is", pi, "we all cheer for the", baseball_team, "the best color is" ,color, "your quest is currently", quest, "and you must know the velocity of an unlaiden swallow is", velocity)
