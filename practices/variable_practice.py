ef936156cd92413e55084c94b8c70f72b9e81c3e
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-08-31 22:54:51 +0000

    added varaibles and prints

--- practices/variable_practice.py
+++ practices/variable_practice.py
@@ -1,4 +1,37 @@
 # SP 6Th Variables Practice
 
-name = input("What is your name?")
+name = "Santiago"
 
+print(name)
+
+number = "7"
+
+print(number)
+
+number = "29"
+
+print(number)
+
+breakfast = "Pancakes, Eggs, and Hashbrowns"
+
+print(breakfast)
+
+color = "Red"
+
+print(color)
+
+school_name = "Utah County Academy of Sciences"
+
+print(school_name)
+
+year = "2025"
+
+print(year)
+
+age = "Fourteen"
+
+print(age)
+
+favorite_subject = "Chemistry"
+
+print(favorite_subject)
