b1a290862d4b5f1f4293dd8fd2a93eff786347a9
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-04 13:43:50 -0600

    Started expression notes

---
+++ notes/expressions.py
@@ -0,0 +1,32 @@
+#SP 6Th expression notes
+
+# Algorithm
+name = input("what is your name?")
+print("hello", name)
+
+#algorithm for area
+length = 5
+width = 20
+area = length * width 
+print(length, "*", width, "=", area)
+
+#problem to solve, average age of a group of 4.
+age_1 = 14
+age_2 = 15
+age_3 = 13
+age_4 = 14
+average_1 = age_1 + age_2 + age_3 + age_4 
+average_2 = average_1 /4
+print("The average age between:", age_1,"," , age_2,"," , age_3,"and", age_4, "=" ,average_2)
+
+#Math equations
+num_1 = 12
+num_2 = 8
+
+print("addition:" ,num_1 + num_2)
+print("subtraction:" ,num_1 - num_2)
+print("multiplication:" ,num_1 * num_2)
+print("division:", num_1 / num_2)
+print("exponents:", num_1 ** num_2)
+print("interger division:", num_1 // num_2)
+print("modulo (remainder):", num_1 % num_2)
