a77b95eb5aa7630d9bbc6fd51facfcb72e1cf627
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-08 13:44:42 -0600

    Worked on string notes

--- notes/string.py
+++ notes/string.py
@@ -1,1 +1,13 @@
 #SP 6TH String notes
+
+#Examples of strings
+first_name = input("What is your first name:\n").strip().title()
+
+last_name = input("What is your last name:\n").strip().title()
+
+full_name = first_name + " " + last_name
+
+sentence = "the quick brown fox jumps oveer the lazy dog"
+
+print("welcome to my program",full_name + "!")
+#strip removes any unnecessary spaces and upper turns every letter upper case and .lower turns everything lowercase, captialzie turns the first letter upper case, title turns the first letter of every word into uppercase
