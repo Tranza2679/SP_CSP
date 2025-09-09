407c8321fe5be3f350cf88995795a6ce887a57da
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-09 16:34:42 +0000

    Worked on Financial caculator

--- practices/financial_calculator.py
+++ practices/financial_calculator.py
@@ -2,3 +2,9 @@
 
 name = input("Hello please write your name").strip().title()
 print("Hello", name,"welcome to Financial calculator!")
+
+monthly_income = input("Please write your monthly income")
+
+monthly_rent = input("Please write your monthly rent")
+
+monthly_utilities = input("please write your monthly utilities") 
