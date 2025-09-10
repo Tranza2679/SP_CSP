1a7085c4306161cf664ffe0ffae793dc3e61231b
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-10 13:41:33 -0600

    Started silly sentence generator

---
+++ practices/silly_sentence.py
@@ -0,0 +1,9 @@
+#SP 6th Silly sentence generator
+print("Hello Welcome To Silly sentence Generator!")
+name = input("Please write a name of your choosing!").strip()
+verb = input("Please write a verb of your choosing!")
+adjective = input("Please write a adjective of your choosing!")
+location = input("Please write a location of your choosing")
+plural = input("Please write a plural object of your choosing!")
+
+print(name,"was",verb,"to the ")
