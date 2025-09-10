bb0de84ac978c1f33b1dbe865f5c32027efd6921
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-10 13:27:40 -0600

    Made and Finished name decorator

---
+++ practices/name_decorator.py
@@ -0,0 +1,5 @@
+#SP 6th Name decorator
+print("Hello Welcome to name decorator!")
+name = input("Please enter your name!").strip().title()
+print("*(^v^)*"+name+"*('w')*")
+print("<: :P "+name+" q: :>")
