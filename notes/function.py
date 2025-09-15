428598b81f27380a7a2ccd41b525c9d2f2c52a0d
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-15 13:43:50 -0600

    Worked on Function Notes

---
+++ notes/function.py
@@ -0,0 +1,50 @@
+#SP 6th Function notes
+
+
+import random
+
+
+def welcome():
+    name = input("What is your name\n")
+    print(f"Hello {name}!")
+
+print(f"The function is over!")
+
+welcome()
+
+
+def add(number,numer_two): #parameters are given when we define the function 
+    number =number + num_two
+    print(number)
+
+num_one = 12
+num_two = 14
+add(num_one, num_two)#arguments are given when we call the function
+add(4,8)
+add(9,700)
+add(87, 45)
+
+def clean(info):
+    return info.strip().lower()
+
+name = input("What is your name?")
+quest = input("What is your quest?")
+color = input("What is your favorite color?")
+
+print(f"Hello, {clean(name)}. I have heard you are trying to {clean(quest)}, that is super cool! Are you sure {clean(color)} is your favorite color?")
+
+ def believe(sentence):
+   length = len(sentence)
+   spot_one = random.randint(0, length - 1)
+   spot_two = random.randint(0, length - 1)
+   spot_three = random.randint(0, length - 1)
+   full_sentence = sentence.split(" ")
+   full_sentence.insert(spot_one, "Believe it!")
+   full_sentence.insert(spot_two, "Believe it!")
+   full_sentence.insert(spot_three, "Believe it!")
+   sentence = full_sentence.join("")
+   return sentence
+
+new_senetnce = believe("This quick brown fox jumps over the lazy dog!")
+print(new_sentence)
+print
--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -5,5 +5,7 @@ verb = input("Please write a verb that ends with ing of your choosing!")
 adjective = input("Please write a adjective of your choosing!")
 location = input("Please write a location of your choosing")
 plural = input("Please write a plural object of your choosing!")
+creature = input("please write the name of a creature or animal of your choosing!").strip().title()
 
-print(name,"was",verb,"to the ")
+print(name,"was",verb,"to the",adjective,location,"where they were furiously pelted with",plural,"by a bunch of",creature + "s.")
+print("Thank you for using silly sentence generator!")
