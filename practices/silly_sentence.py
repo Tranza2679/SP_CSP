73099d1b963856dbd99dbaaa3669e8238b9308f0
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-15 12:51:03 -0600

    Silly sentence

--- practices/silly_sentence.py
+++ practices/silly_sentence.py
@@ -1,11 +1,9 @@
 #SP 6th Silly sentence generator
 print("Hello Welcome To Silly sentence Generator!")
-name = input("Please write a name of your choosing!").strip().title()
-verb = input("Please write a verb ending with ing of your choosing!").strip().title()
-adjective = input("Please write a adjective of your choosing!").strip().title()
-location = input("Please write a location of your choosing").strip().title()
-plural = input("Please write a plural object of your choosing!").strip().title()
-creature =input("please write the name of a creature or animal of your choosing!").strip().title()
+name = input("Please write a name of your choosing!").strip()
+verb = input("Please write a verb that ends with ing of your choosing!")
+adjective = input("Please write a adjective of your choosing!")
+location = input("Please write a location of your choosing")
+plural = input("Please write a plural object of your choosing!")
 
-print(name,"was",verb,"to the",adjective,location,"where they were furiously pelted with",plural,"by a bunch of",creature + "s.")
-print("Thank you for using silly sentence generator!")
+print(name,"was",verb,"to the ")
