c16c90a3b2290c10ca7e49e1db3734328f3cf3bb
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-18 13:33:57 -0600

    Finished conditional notes along with old enough

--- notes/conditional.py
+++ notes/conditional.py
@@ -15,7 +15,23 @@ name = input("Please tell me your name: ")
 
 if name == "Ms LaRose": #You want to write your conditionals from least likely to most likely
     print("You are the teahcer!")
+    if num == 4: 
+        print("That is my favorite number!")
 elif name == "Tia": #elif means else if 
     print("You are the TA!")
 else: 
+    if name == "Lucas":
+        print("You aare in 6th period!")
     print(f"Hello {name}, you are a student!")
+
+#AND OR NOT
+
+if num >=0 and num < 10: #and means both must be true
+    print(f"{num} is a single digit number!")
+elif num < 25 or num == 50: #or means only one must be true
+    print(f"Wow {num} is a really cool number!")
+elif not num < 100: #not checks if the opposite is true
+    print(f"{num} is a really large number")
+else:
+    print(f"You typed in a {num}")
+
---
+++ practices/old_enough.py
@@ -0,0 +1,12 @@
+#SP 6th Old Enough 
+
+age = int(input("How old are you?"))
+
+if age >= 18: 
+    print("You are old enough to vote!")
+elif age >= 16:
+    print("You are old enough to be able to drive!") 
+elif age >= 15:
+    print("You are old enough to get a learners permit!")
+else:
+    print("You are old enough to be able to go to school!")
