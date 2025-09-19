907646dae3956cd463f8aad4181a7c58ac46e269
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-19 13:41:57 -0600

    Started for loops and finished time of day

---
+++ notes/for_loops.py
@@ -0,0 +1,15 @@
+#SP 6th Time and for loops notes!
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+
+print(f"The time is: {current}")
+print(f"The hour is: {hour}")
+
+# Lists/ first type of complex data time they are surrounded by []
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia","Treyson", "Xavier", "Jake"["Joseph", "Noah", "Zee"], 8, 3.4158]
+
+
--- practices/time.py
+++ practices/time.py
@@ -1,2 +1,15 @@
 #SP 6th Time of day 
-time = input("Hello please write the current time!")
+
+import datetime
+
+current = datetime.datetime.now()
+hour = current.hour
+
+if hour >= 18:
+    print(f"Hello the time is {hour}, good evening!")
+elif hour > 12 and hour < 18:
+    print(f"Hello The time is {hour}, good afternoon!")
+elif hour >= 6 and hour < 12:
+    print(f"Hello the time is {hour}, good morning!")
+
+
