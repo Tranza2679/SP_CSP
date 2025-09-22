9642c6b8792ed54973cc289038f964df9ba189c2
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-09-22 13:41:28 -0600

    Finished Loop notes

--- notes/for_loops.py
+++ notes/for_loops.py
@@ -10,6 +10,50 @@ print(f"The time is: {current}")
 print(f"The hour is: {hour}")
 
 # Lists/ first type of complex data time they are surrounded by []
-siblings = ["Alex", "Katie", "Andrew", "Vienna", "Tia","Treyson", "Xavier", "Jake"["Joseph", "Noah", "Zee"], 8, 3.4158]
+siblings = ["Alex", "Katie", "Andrew", "Vienna", "Victoria","Treyson", "Jefferson", "Jake"]
 
+print(siblings[3])
+print(siblings)
+siblings[4] = "Tia" 
+siblings[6] = "Xavier"
+siblings.remove("Vienna")
+print(siblings)
 
+#For loop
+for sibling in siblings:
+    #print(f"Hello {sibling}")
+    print("Hi")
+
+for x in range(20,-11, -1): #the first number is what we start with, second is what we end with and the third is what it counts by
+    print(x)
+
+#while loops
+#Infinite loop 
+#while True:
+    #print("Oh No!")
+
+#Proper while loop
+i = 1
+
+while i < 21:
+    if i % 2 == 0:
+        print(f"{i} is even")
+    else:
+        print(f"{i} is odd")
+    i += 1
+
+import random
+
+number = random.randint(1,20)
+x = 1
+#"print("Goose!")"
+
+while True:
+    if number == x:
+        print("Goose!")
+        break
+    else:
+        print("Duck")
+        x += 1 
+#break breaks the loop
+    
--- practices/time.py
+++ practices/time.py
@@ -9,7 +9,7 @@ if hour >= 18:
     print(f"Hello the time is {hour}, good evening!")
 elif hour > 12 and hour < 18:
     print(f"Hello The time is {hour}, good afternoon!")
-elif hour >= 6 and hour < 12:
+elif hour >= 1 and hour < 12:
     print(f"Hello the time is {hour}, good morning!")
 
 
