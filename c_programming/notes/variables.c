47eebce33feb648c2d98c0c1bc6d8499cfcf5afc
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-02 13:44:07 -0600

    Made variable notes

---
+++ c_programming/notes/variables.c
@@ -0,0 +1,13 @@
+// SP 6th Variables Notes;
+#include <stdio.h>
+
+int main(void){
+    int num = 4;
+    float pi = 3.14;
+    char grade = 'A'; //will only hold 1 letter 
+    char name[] = "LaRose";
+    //bool passing = true;
+    printf("%d", num);
+
+    return 0;
+}
