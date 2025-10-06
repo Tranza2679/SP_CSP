199d44b9cb2953c95b442ce36f78f5e49182758b
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-06 13:44:09 -0600

    Moved practices and started variable practice

---
+++ c_programming/practices/variable_practice.c
@@ -0,0 +1,16 @@
+// SP 6th Variable Practice 
+
+#include <stdio.h>
+
+int main(void){
+    
+    char name[15] = "Santiago Pineda";
+    printf("My name is %s", name);
+
+    int num = 6;
+    printf("A number I like is %d", num);
+
+    double gpa = 3.87;
+    printf("A random GPA would be ");
+    return 0;
+}
