010f6e524b39c753e4294293fbfd04f3732e1cd2
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-07 02:08:45 +0000

    Finished variavle practice

--- c_programming/practices/variable_practice.c
+++ c_programming/practices/variable_practice.c
@@ -5,12 +5,35 @@
 int main(void){
     
     char name[15] = "Santiago Pineda";
-    printf("My name is %s", name);
+    printf("My name is %s\n", name);
 
     int num = 6;
-    printf("A number I like is %d", num);
+    printf("A number I like is %d\n", num);
 
-    double gpa = 3.87;
-    printf("A random GPA would be ");
+    float gpa = 3.87;
+    printf("A random GPA would be %f\n", gpa);
+
+    char breakfast[32] = "Eggs";
+    printf("For breakfast today I had %s\n", breakfast);
+
+    char color[32] = "Red";
+    printf("My favorite color is %s\n", color);
+
+    char school_name[32] = "Utah County Academy of Sciences";
+    printf("My school is %s\n", school_name);
+
+    int year = 2025;
+    printf("The year is %d\n", year);
+
+    char eye_color[32] = "Brown";
+    printf("My eye color is %s\n", eye_color);
+
+    int age = 15;
+    printf("My age is %d\n", age);
+
+    char fav_subject[32] = "Chemistry";
+    printf("My favorite subject is %s\n", fav_subject);
+
+
     return 0;
 }
