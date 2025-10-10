9e873bfc89cb065d31adf2b8e6e0d72757d94292
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-10 18:17:53 +0000

    Worked on silly sentence generator

--- c_programming/practices/silly_sentence.c
+++ c_programming/practices/silly_sentence.c
@@ -5,11 +5,17 @@
 int main(void){
     char name[50];
     char verb[50];
-    char adjective;
-    char location;
-    char plural;
-    char creature
-    prinf("Welcome to Silly Sentence Generator!")
+    char adjective[50];
+    char location[50];
+    char plural[50];
+    char creature[50];
+    prinf("Welcome to Silly Sentence Generator!");
+
+    printf("PLease write a name of your choosing:\n");
+    scanf("%s", name);
+
+    print("Please write a verb of your choosing:\n")
+    scanf("%s", verb)
 
     return 0;
 }
