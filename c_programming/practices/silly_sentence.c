1b93d20f43fe42ba5a732712be445f22d427127f
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-10 17:47:54 +0000

    Finished name decorator and started sillly sentence generator

---
+++ c_programming/practices/silly_sentence.c
@@ -0,0 +1,15 @@
+// SP 6th Silly Sentence Generator
+#include <stdio.h>
+#include <string.h>
+
+int main(void){
+    char name[50];
+    char verb[50];
+    char adjective;
+    char location;
+    char plural;
+    char creature
+    prinf("Welcome to Silly Sentence Generator!")
+
+    return 0;
+}
