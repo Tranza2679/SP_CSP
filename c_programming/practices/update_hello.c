bfe35f599db909ca30e67681128988bf90c04305
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-16 00:47:12 +0000

    changeda program

--- c_programming/practices/update_hello.c
+++ c_programming/practices/update_hello.c
@@ -2,18 +2,15 @@
 
 #include <stdio.h>
 
-void hello(){
-    char name[50];
-    printf("PLease write your name: ");
-    scanf("%s", name);
+void hello(char* name){
     printf("Hello %s!\n", name);
 }
 int main(void){
-   hello();
-   hello();
-   hello();
-   hello();
-   hello();
+   hello("Santiago");
+   hello("Jimbob");
+   hello("Ayin");
+   hello("Johnny");
+   hello("Susie");
    
     return 0;
 }
