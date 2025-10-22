c0caa7f091b5846c7b70a1bba99d02313e5cbf96
Tranza2679 <santiago.pineda@ucas-edu.net>
2025-10-22 13:43:55 -0600

    Boo,

---
+++ c_programming/notes/rancom.c
@@ -0,0 +1,15 @@
+//random
+#include <stdio.h>
+#include <time.h>
+#include <stdlib.h>
+
+int main(void){
+    char letters[] ={'a', 'b', 'c', 'd', 'e'};
+    srand(time(NULL));
+    for(int i=0; i < 10; i++){
+    int random = rand() %5;
+    printf("%c\n", letters[random]);
+    }
+
+    return 0;
+}
